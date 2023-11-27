32cabb3f68e98d488800209e3727849a8d05d5e4
David Anderson <113274870+anderd84@users.noreply.github.com>
2023-11-27 14:16:58 -0700

    Add files via upload

---
+++ ROCKET.m
@@ -0,0 +1,30 @@
+Deneb.NAME = "Deneb 2";
+
+Deneb.NOSECONE_LENGTH = 34.64; %in
+Deneb.DIAMETER = 8; %in
+
+Deneb.BODY_LENGTH = 221; %in
+
+Deneb.FIN.ROOT_CHORD = 28; %in
+Deneb.FIN.TIP_CHORD = 13; %in
+Deneb.FIN.SPAN = 9; %in
+Deneb.FIN.SWEEP_LENGTH = 7.5; %in
+Deneb.FIN.THICKNESS = .25; %in
+Deneb.FIN.LEADING_EDGE_PERCENT_ROOT = 1/28; %
+Deneb.FIN.TRAILING_EDGE_PERCENT_ROOT = 1/28; %
+
+Deneb.ENGINE.EXIT_AREA = 7.5^2*pi/4; %in2
+Deneb.ENGINE.MDOT_0 = 7.5; %lbm/s
+Deneb.ENGINE.ASTAR_0 = 2.36^2*pi/4; %in2
+Deneb.ENGINE.THROAT_REGRESSION_RATE = 0.001; %in/s
+
+Deneb.MASS_INITIAL = ;
+Deneb.MASS_FINAL = ;
+
+Deneb.CHUTE.CD_MAIN = 0;
+Deneb.CHUTE.AREA_MAIN = 0;
+Deneb.CHUTE.CD_DROGUE = 0;
+Deneb.CHUTE.AREA_DROGUE = 0;
+
+
+
